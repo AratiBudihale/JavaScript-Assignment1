08624c7c7186de5c4495b2cb3a6fc104c3ea7da5
AratiBudihale <73887532+AratiBudihale@users.noreply.github.com>
2021-06-28 15:47:52 +0530

    Add files via upload

---
+++ Assignment1-1.js
@@ -0,0 +1,19 @@
+
+
+var str ="Arati Budihale";
+var final = str.toLowerCase();
+var newObject = {};
+
+for( i in final)
+{
+    if(newObject.hasOwnProperty(final[i]))
+    {
+        newObject[final[i]]+=1;
+    }
+    else
+    {
+        newObject[final[i]] = 1;
+    }
+        
+}
+console.log(newObject);
---
+++ Assignment1-2.js
@@ -0,0 +1,9 @@
+
+const arr = [3, 5, 7, 9];
+
+const narr=arr.map(arr=>arr*2)
+
+console.log(narr)
+
+
+
